42d0b6cd5ed6d84c930e28f930618ced91cbc901
Petter Eriksson <periksson@splunk.com>
2013-09-18 11:56:30 -0800

    adds httpkit

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
                  [compojure "1.1.5"]
                  [org.apache.hadoop/hadoop-core "1.1.2"]
                  [org.apache.commons/commons-compress "1.5"]
-                 [midje "1.5.1"]]
+                 [midje "1.5.1"]
+                 [http-kit "2.1.10"]]
   :plugins [[lein-ring "0.8.5"]]
   :ring {:handler images.handler/app}
   :profiles
