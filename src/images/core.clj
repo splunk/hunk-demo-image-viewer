d85b50f93b881bdfbe16f521639e35371b0b15d4
Petter Eriksson <periksson@splunk.com>
2013-09-23 15:40:35 -0800

    hdfs uri is passed as the 1st argument

--- src/images/core.clj
+++ src/images/core.clj
@@ -1,8 +1,10 @@
 (ns images.core
   (:require [org.httpkit.server :as s]
+            [images.hdfsreader :as hdfs]
             [images.handler :as h]))
 
 (def port 5123)
 
-(defn -main []
+(defn -main [hdfs-uri]
+  (reset! hdfs/hdfs-uri hdfs-uri)
   (s/run-server h/app {:port port}))
--- src/images/hdfsreader.clj
+++ src/images/hdfsreader.clj
@@ -1,6 +1,8 @@
 (ns images.hdfsreader
   (:import [com.splunk.hunk.input.entry TarEntryReader MapEntryReader]))
 
+(def hdfs-uri (atom nil))
+
 (defn get-entry-reader [path]
   (if (re-matches #".*\.map/data" path)
     (MapEntryReader.)
@@ -10,6 +12,6 @@
 
 (defn get-image [hdfspath filename]
   (.getHdfsFileStream (get-entry-reader hdfspath)
-                      "hdfs://localhost:9000"
+                      @hdfs-uri
                       hdfspath
                       filename))
